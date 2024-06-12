d89ed5260a8b2ae241c8a2b63767203febef65dd
NauvalHamzah <nauvalhamzah33@gmail.com>
2024-06-13 06:48:29 +0700

    Update Code

--- Challange11.js
+++ Challange11.js
@@ -2,7 +2,8 @@ const { readFileSync } = require('fs');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt: 'Tebakan: '
 })
 
 isiData = JSON.parse(readFileSync('data.json', 'utf8'))
@@ -10,21 +11,24 @@ isiData = JSON.parse(readFileSync('data.json', 'utf8'))
 console.log("Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya!")
 
 function tebakKata(i = 0) {
-  if (i >= isiData.length) {
-    console.log("Hore Anda menang!")
-    rl.close();
-  } else {
-    rl.question('Pertanyaan: ' + isiData[i].definition + '\nJawaban: ', (tebakan) => {
-      if (tebakan === isiData[i].term) {
-        console.log("Selamat Anda benar!\n");
-        i++;
-        tebakKata(i);
+  console.log(`Pertanyaan: ${isiData[i].definition}`)
+  rl.prompt()
+  rl.on('line', (tebakan) => {
+    if (tebakan === isiData[i].term) {
+      console.log("Selamat Anda benar!\n");
+      i++;
+      if (i >= isiData.length) {
+        console.log("Hore Anda menang!")
+        rl.close();
       } else {
-        console.log("Wkwkwk, Anda kurang beruntung!\n");
-        tebakKata(i);
+        console.log(`Pertanyaan: ${isiData[i].definition}`)
+        rl.prompt()
       }
-    });
-  }
+    } else {
+      console.log("Wkwkwk, Anda kurang beruntung!\n");
+      rl.prompt()
+    }
+  });
 }
 
 tebakKata()
