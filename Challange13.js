a66c5c6af5cead6708ad35ac044395bf6a610879
NauvalHamzah <nauvalhamzah33@gmail.com>
2024-06-14 01:46:50 +0700

    Update Code

--- Challange13.js
+++ Challange13.js
@@ -1,18 +1,18 @@
 //Import Library
+const { readFileSync, writeFileSync } = require('fs');
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-let allTask = []; //Main Array
-
 //object construction
 function taskList(title, complete = false, tags = []) {
     this.title = title;
     this.complete = complete;
     this.tags = tags;
-    this.status = function () { return this.complete ? "[x]" : "[ ]"; }
+    this.status = function() {
+        return this.complete? "[x]":"[ ]"}
     this.checklist = this.status()
 }
 
@@ -43,7 +43,7 @@ function handleList() {
 function handleTask(taskNo) {
     taskNo = taskNo - 1;
     if (typeof (taskNo) === 'number' && taskNo < allTask.length) {
-        mainKeys = ["title", "complete", "tags"]
+        mainKeys = ["title", "complete", "tags","checklist"]
         mainKeys.forEach(key => {
             console.log(`${key}: ${allTask[taskNo][key]}`)
         });
@@ -67,8 +67,8 @@ function handleDelete(taskNo) {
 function handleComplete(taskNo) {
     taskNo = taskNo - 1;
     if (typeof (taskNo) === 'number' && taskNo < allTask.length) {
-        allTask[taskNo]["complete"] = true;
-        allTask[taskNo]["checklist"] = allTask[taskNo].status()
+        allTask[taskNo].complete = true;
+        allTask[taskNo].checklist = "[x]"
         console.log(`"${allTask[taskNo]["title"]}" telah selesai`)
     } else { handleError() }
 }
@@ -76,8 +76,8 @@ function handleComplete(taskNo) {
 function handleUncomplete(taskNo) {
     taskNo = taskNo - 1;
     if (typeof (taskNo) === 'number' && taskNo < allTask.length) {
-        allTask[taskNo]["complete"] = false;
-        allTask[taskNo]["checklist"] = allTask[taskNo].status()
+        allTask[taskNo].complete = false;
+        allTask[taskNo].checklist = "[ ]";
         console.log(`"${allTask[taskNo]["title"]}" status selesai dibatalkan`)
     } else { handleError() }
 }
@@ -166,7 +166,7 @@ function executeCommand(command, argument) {
             promptCommand();
             break;
         case 'task':
-            handleTask();
+            handleTask(argument);
             promptCommand();
             break;
         case 'add':
@@ -213,7 +213,18 @@ function promptCommand() {
         const { command, argument } = parseCommand(input);
         executeCommand(command, argument);
     });
+
+    rl.on('close', () => {
+        console.log('good bye!');
+        writeFileSync('dataC13.json', JSON.stringify(allTask), 'utf8')
+        process.exit(0);
+    });
 }
 
+try {
+    allTask = JSON.parse(readFileSync("dataC13.json", 'utf8'));
+  } catch (err) {
+    allTask = [];
+}
 // Start the command prompt
 promptCommand();
