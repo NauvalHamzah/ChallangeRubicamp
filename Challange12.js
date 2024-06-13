c93363a1343212c304872f3992d38f1937e71640
NauvalHamzah <nauvalhamzah33@gmail.com>
2024-06-13 08:02:15 +0700

    Update code

--- Challange12.js
+++ Challange12.js
@@ -2,57 +2,59 @@ const { readFileSync } = require('fs');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt:"Jawaban: "
 })
 
 function tebakKata(isiData, i = 0, wrongAns = 0) {
-  if (i >= isiData.length) {
-    console.log("Anda berhasil!");
-    rl.close();
-  } else {
-    rl.question('Pertanyaan: ' + isiData[i].definition + '\nJawaban: ', (tebakan) => {
-      if (tebakan === 'skip') {
-        temp = isiData.splice(i, 1)[0];
-        isiData.push(temp);
-        console.log("\n")
-        tebakKata(isiData, i);
-      } else if (tebakan === isiData[i].term) {
-        console.log("Anda beruntung!\n");
-        i++;
-        tebakKata(isiData, i);
+  console.log(`Pertanyaan: ${isiData[i].definition}`)
+  rl.prompt()
+  rl.on('line', (tebakan) => {
+    if (tebakan == "skip"){
+      temp = isiData.splice(i, 1)[0];
+      isiData.push(temp);
+      console.log(`\nPertanyaan: ${isiData[i].definition}`)
+      rl.prompt()
+      wrongAns=0;
+    } else if (tebakan === isiData[i].term) {
+      console.log("Selamat Anda benar!\n");
+      i++;
+      if (i >= isiData.length) {
+        console.log("Hore Anda menang!")
+        rl.close();
       } else {
-        wrongAns++;
-        console.log(`Anda kurang beruntung! anda telah salah ${wrongAns} kali, silahkan coba lagi\n`);
-
-        tebakKata(isiData, i, wrongAns);
+        console.log(`Pertanyaan: ${isiData[i].definition}`)
+        rl.prompt()
+        wrongAns=0;
       }
-    });
+    } else {
+      wrongAns++;
+      console.log(`Anda kurang beruntung! anda telah salah ${wrongAns} kali, silahkan coba lagi\n`);
+      rl.prompt()
+    }
+  });
+}
+
+function startHandling(initFile) {
+  try {
+    isiData = JSON.parse(readFileSync(initFile, 'utf8'));
+    console.log(`Selamat datang di permainan Tebak-tebakan. kamu akan diberikan pertanyaan dari file ini ${initFile}.\nUntuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi\n`);
+    tebakKata(isiData);
+  } catch (err) {
+    console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node solution.js data.json'");
   }
 }
 
 function openFile(initFile = '') {
   if (initFile) {
-    try {
-      isiData = JSON.parse(readFileSync(initFile, 'utf8'));
-      console.log(`Selamat datang di permainan Tebak-tebakan. kamu akan diberikan pertanyaan dari file ini ${fileName}.\nUntuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi\n`);
-      tebakKata(isiData);
-    } catch (err) {
-      console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node solution.js data.json'");
-    }
+    startHandling(initFile);
   } else {
     console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node solution.js data.json'");
-    rl.on('line', fileName => {
-      try {
-        isiData = JSON.parse(readFileSync(fileName, 'utf8'));
-        console.log(`Selamat datang di permainan Tebak-tebakan. kamu akan diberikan pertanyaan dari file ini ${fileName}.\nUntuk bermain, jawablah dengan jawaban yang sesuai.\nGunakan 'skip' untuk menangguhkan pertanyaannya, dan di akhir pertanyaan akan ditanyakan lagi\n`);
-        tebakKata(isiData);
-      } catch (err) {
-        console.log("Tolong sertakan nama file sebagai inputan soalnya\nMisalnya 'node solution.js data.json'");
-      }
-
+    rl.on('line', initFile => {
+      startHandling(initFile)
     })
   }
 }
 
-openFile()
+openFile("data.json")
 
